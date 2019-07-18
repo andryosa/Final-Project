bc4e2c067245009a2abde83d06ac318e38e7f326
Andryosa <andryos44@gmail.com>
2019-07-18 14:21:03 +0200

    upload

---
+++ js/ci.js
@@ -0,0 +1,33 @@
+function sd1Click(){
+    document.getElementById("sdpic2").classList.add("zzz");
+    document.getElementById("sdpic3").classList.add("zzz");
+    document.getElementById("sdpic4").classList.add("zzz");
+    document.getElementById("sdpic1").classList.remove("zzz");
+}
+function sd2Click(){
+    document.getElementById("sdpic2").classList.remove("zzz");
+    document.getElementById("sdpic3").classList.add("zzz");
+    document.getElementById("sdpic4").classList.add("zzz");
+    document.getElementById("sdpic1").classList.add("zzz");
+}
+function sd3Click(){
+    document.getElementById("sdpic2").classList.add("zzz");
+    document.getElementById("sdpic3").classList.remove("zzz");
+    document.getElementById("sdpic4").classList.add("zzz");
+    document.getElementById("sdpic1").classList.add("zzz");
+}
+function sd4Click(){
+    document.getElementById("sdpic2").classList.add("zzz");
+    document.getElementById("sdpic3").classList.add("zzz");
+    document.getElementById("sdpic4").classList.remove("zzz");
+    document.getElementById("sdpic1").classList.add("zzz");
+}
+
+function as1Click(){
+    document.getElementById("aspic1").classList.remove("zzz");
+    document.getElementById("aspic2").classList.add("zzz");
+}
+function as2Click(){
+    document.getElementById("aspic1").classList.add("zzz");
+    document.getElementById("aspic2").classList.remove("zzz");
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+let firstname, lastname, fullname;
+let x;
+
+firstname='Andryosa';
+lastname='William';
+fullname="My Name is "+firstname+" "+lastname;
+x=1+7;
+//alert(fullname);
+console.log(fullname);
+console.log(typeof(firstname))
+console.log(x);
+console.log(typeof(x));
+alert("Hi, welcome to my first Website")
